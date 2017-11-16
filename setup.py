b1401a39c87addb099d556dd344ad05fc0a0f6bb
Peng Yu <peng.yu@shopify.com>
2017-11-16 14:39:36 -0500

    d

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ from setuptools import setup
 import os
 
 
-INSTALL_REQUIRES = ['gym==0.1.0', 'cmake==0.8.0', 'tensorflow==1.3.0', "atari-py==0.1.1"]
+INSTALL_REQUIRES = ['gym==0.1.0', 'cmake==0.8.0', 'tensorflow==1.3.0', 'atari-py==0.1.2']
 
 setup(
     name='trainer',
-    version='0.1',
+    version='0.2',
     install_requires=INSTALL_REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    requires=[]
-    dependency_links = ['http://github.com/yupbank/atari-py.git@cb0a4a76c9cde1bd1fe322b50b5c46f07fd85a6e#egg=elasticutils']
-
+    requires=[],
+    dependency_links = ['git+http://github.com/yupbank/atari-py.git@5b877d3ccb317e4bc2f074a54c7509ef09a155cc#egg=atari-py-0.1.2']
 )
